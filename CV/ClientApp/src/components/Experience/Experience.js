bd8a72306cfeb5ecbca966528dd82a4269b298bb
vijayalakshmig <vijaya.laxmi502@gmail.com>
2019-01-10 22:46:56 +0200

    created Experience component

--- CV/ClientApp/src/components/Experience/Experience.js
+++ CV/ClientApp/src/components/Experience/Experience.js
@@ -1,9 +1,32 @@
 ﻿import React, { Component } from 'react';
+import { Col, Grid, Row } from 'react-bootstrap';
+
+import { ContentHeading } from '../ContentHeading/ContentHeading';
 
 export class Experience extends Component {
     displayName = Experience.name;
 
     render() {
-
+        return (
+            <div>
+                <ContentHeading name="Experience" />
+                <h4>{this.props.experience.title}</h4>
+                <h4><b>{this.props.experience.company}</b></h4>
+                <Row>
+                    <Col xs={6} md={6}>
+                        <i className="fa fa-calendar"></i> {this.props.experience.from} - {this.props.experience.to}
+                    </Col>
+                    <Col xs={6} md={6}>
+                        <i className="fa fa-map-marker"></i> {this.props.experience.location}
+                    </Col>
+                </Row>
+                <br/>
+                <ul>
+                    {this.props.experience.responsibilities.map(function (name, index) {
+                        return <li key={index}>{name}</li>;
+                    })}
+                </ul>
+            </div>
+        );
     }
 }
--- CV/ClientApp/src/components/MyCV/MyCv.js
+++ CV/ClientApp/src/components/MyCV/MyCv.js
@@ -6,33 +6,43 @@ import { ContentHeading } from './../ContentHeading/ContentHeading';
 import personalPhoto from '../../assets/img/SamImg.jpg'
 
 import './MyCV.css';
+import { Experience } from '../Experience/Experience';
 
 
 export class MyCv extends Component {
     displayName = MyCv.name;
 
     render() {
+        var experience1 = {
+            title:  "President & CEO",
+            company: "Yahoo!",
+            from: "july 2012",
+            to: "Ongoing",
+            location: "Sunnyvale, CA",
+            responsibilities: [
+                "Led the $5 billion acquisition of the company with Verizon - the entity which believed most in the immense value Yahoo! has created",
+                "Built Yahoo's mobile, video & social businesses from nothing in 2011 to $1.6 billion in GAAP revenue in 2015",
+                "Tripled the conpany's mobile base to over 600 million monthly active users and generated over $1 billion of mobile advertising revenue last year"]
+        }
+
         return (
             <div className="page">
                 <Grid fluid>
                     <Row>
-                        <Col xs={6} md={4} xsOffset={2}> 
-                            <ContentHeading name="Vijaya Lakshmi" /> 
+                        <Col xs={6} md={6} xsOffset={1}> 
+
                         </Col>
-                        <Col xs={6} md={6}>
+                        <Col xs={6} md={4} xsOffset={1}>
                             <div>
                                 <img src={personalPhoto} className="responsive" alt="my-photo" />
                             </div>
                         </Col>
                     </Row>
                     <Row>
-                        <Col xs={6} md={4} xsOffset={2}>
-                            <ContentHeading name="Experience" />
+                        <Col xs={6} md={7} xsOffset={1}>
+                            <Experience experience={experience1} />
                         </Col>
-                        <Col xs={6} md={6}>
-                            <div>
-                                <img src={personalPhoto} className="responsive" alt="my-photo" />
-                            </div>
+                        <Col xs={6} md={3} xsOffset={1}>
                         </Col>
                     </Row>
                 </Grid>
