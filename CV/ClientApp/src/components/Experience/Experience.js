9d0f8986250719f8432fea3fa93420bed6417f5a
vijayalakshmig <vijaya.laxmi502@gmail.com>
2019-01-11 13:46:41 +0200

    Added experience content part.

--- CV/ClientApp/src/components/Experience/Experience.js
+++ CV/ClientApp/src/components/Experience/Experience.js
@@ -9,7 +9,6 @@ export class Experience extends Component {
     render() {
         return (
             <div>
-                <ContentHeading name="Experience" />
                 <h4>{this.props.experience.title}</h4>
                 <h4><b>{this.props.experience.company}</b></h4>
                 <Row>
@@ -20,10 +19,10 @@ export class Experience extends Component {
                         <i className="fa fa-map-marker"></i> {this.props.experience.location}
                     </Col>
                 </Row>
-                <br/>
+                <br />
                 <ul>
                     {this.props.experience.responsibilities.map(function (name, index) {
-                        return <li key={index}>{name}</li>;
+                        return <li key={index}>{name}</li>
                     })}
                 </ul>
             </div>
--- CV/ClientApp/src/components/MyCV/MyCv.js
+++ CV/ClientApp/src/components/MyCV/MyCv.js
@@ -14,7 +14,7 @@ export class MyCv extends Component {
 
     render() {
         var experience1 = {
-            title:  "President & CEO",
+            title: "President & CEO",
             company: "Yahoo!",
             from: "july 2012",
             to: "Ongoing",
@@ -25,11 +25,58 @@ export class MyCv extends Component {
                 "Tripled the conpany's mobile base to over 600 million monthly active users and generated over $1 billion of mobile advertising revenue last year"]
         }
 
+        var experience2 = {
+            title: "Vice President of Location & Local Services",
+            company: "Google!",
+            from: "Oct 2010",
+            to: "July 2012",
+            location: "9 Palo Alto.CA",
+            responsibilities: [
+                "Positioned Google Maps as the world leader in moble maps and navigation",
+                "Oversaw 1000• engineers and product managers workingon Google Maps.Google Places and Google Earth.",
+            ]
+        }
+
+        var experience3 = {
+            title: "Vice President of Search Products & UX",
+            company: "Google!",
+            from: "Oct 2005",
+            to: "July 2010",
+            location: "Palo Alto.CA",
+            responsibilities: []
+        }
+
+        var experience4 = {
+            title: "Product Manager & Technical UILead",
+            company: "Google!",
+            from: "Oct 2001",
+            to: "July 2005",
+            location: "Palo Alto.CA",
+            responsibilities: [
+                "Appointed by the founder Larry Page in 2011 to lead the Product Management and UserInteraction teams",
+                "Optimized Google's homepage and A/B tested every minor detailto increase usability (incl. spacing betweenwords,color schemes and pixel-by-pixelelement alignment)",
+            ]
+        }
+
+        var experience5 = {
+            title: "Product Engioeer",
+            company: "Google!",
+            from: "June 1999",
+            to: "2001",
+            location: "Palo Alto.CA",
+            responsibilities: [
+                "Joined the company as employee #20 and female employee #1",
+                "Developed targeted advert sementin order to use users·search queries and show them related ads",
+            ]
+        }
+
+        var experiences = [experience1, experience2, experience3, experience4, experience5]
+
         return (
             <div className="page">
                 <Grid fluid>
                     <Row>
-                        <Col xs={6} md={6} xsOffset={1}> 
+                        <Col xs={6} md={6} xsOffset={1}>
 
                         </Col>
                         <Col xs={6} md={4} xsOffset={1}>
@@ -40,7 +87,10 @@ export class MyCv extends Component {
                     </Row>
                     <Row>
                         <Col xs={6} md={7} xsOffset={1}>
-                            <Experience experience={experience1} />
+                            <ContentHeading name="Experience" />
+                            {experiences.map(function (name, index) {
+                                return ([<Experience key={index} experience={name} /> , <hr className="style3" />]);
+                            })}
                         </Col>
                         <Col xs={6} md={3} xsOffset={1}>
                         </Col>
