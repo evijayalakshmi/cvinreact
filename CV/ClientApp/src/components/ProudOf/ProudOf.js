d64df68337cc395fa95b27479a170de04e1ef155
vijayalakshmig <vijaya.laxmi502@gmail.com>
2019-01-12 16:50:49 +0200

    ProudOf component

---
+++ CV/ClientApp/src/components/ProudOf/ProudOf.js
@@ -0,0 +1,23 @@
+﻿import React, { Component } from 'react';
+import { Col, Grid, Row } from 'react-bootstrap';
+import './ProudOf.css';
+
+export class ProudOf extends Component {
+    dispalyName = ProudOf.name;
+
+    render() {
+
+        return (
+            <Row>
+                <Col md={2}>
+                    <i className={this.props.moment.icon} />
+                </Col>
+                <Col md={10}>
+                    <h5 className="heading5"><b>{this.props.moment.heading} </b></h5>
+                    <p className="text">{this.props.moment.content}</p>
+                </Col>
+            </Row>
+        );
+    }
+
+}
