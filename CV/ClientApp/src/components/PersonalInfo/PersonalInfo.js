a9fea2e305a3817d7f9d62480c7df692224a85d6
vijayalakshmig <vijaya.laxmi502@gmail.com>
2019-01-12 16:51:43 +0200

    PersonalInfo component

---
+++ CV/ClientApp/src/components/PersonalInfo/PersonalInfo.js
@@ -0,0 +1,29 @@
+﻿import React, { Component } from 'react';
+import { Col, Grid, Row } from 'react-bootstrap';
+
+
+export class PersonalInfo extends Component {
+    displayName = PersonalInfo.name;
+
+
+    render() {
+        return (
+            <div>
+                <h1><b> {this.props.info.Name.toUpperCase()}</b></h1>
+                <h4> {this.props.info.Designation} </h4>
+                <Row>
+                    <Col xs={4} md={4}>
+                        <i className="fa fa-at"></i> {this.props.info.Email}
+                    </Col>
+                    <Col xs={4} md={5}>
+                        <i className="fa fa-map-marker"></i> {this.props.info.blog}
+                    </Col>
+                    <Col xs={4} md={3}>
+                        <i className="fa fa-map-marker"></i> {this.props.info.Address}
+                    </Col>
+                </Row>
+                <br />
+            </div>
+        );
+    }
+}
