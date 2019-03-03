690ebf2c099c9d90a0ecf1ecec86a54ef640937c
vijayalakshmig <vijaya.laxmi502@gmail.com>
2019-03-03 23:15:32 +0200

    Introduced optional delete.

--- CV/ClientApp/src/components/Common/IconFieldGroup.js
+++ CV/ClientApp/src/components/Common/IconFieldGroup.js
@@ -12,7 +12,7 @@ export class IconFieldGroup extends Component {
 
         return (
             <div className="form-group">
-                <label htmlFor={this.props.id}>{this.props.label}</label>
+                {this.props.label && <label htmlFor={this.props.id}>{this.props.label}</label>}
                 <div className="input-group">
                     <div className="input-group-prepend">
                         <span className="input-group-text" id="basic-addon1">
@@ -20,6 +20,7 @@ export class IconFieldGroup extends Component {
                         </span>
                     </div>
                     <input type="text" className="form-control" id={this.props.id} {...this.props} autoComplete="off" />
+                    {this.props.delete && [<hr/>, <button className="btn btn-info" onClick={(e) => this.props.delete(e)}><i class="fa fa-trash"></i>Delete</button>]}
                 </div>
                 {this.props.help && <small id="help" className="form-text text-muted">{this.props.help}</small>}
                 {this.props.error && <small id="help" className="form-text text-error" style={errorStyle}>{this.props.error}</small>}
