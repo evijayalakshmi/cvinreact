87dc0fec1e619b4bcf458d3c380f7427ae135d4f
vijayalakshmig <vijaya.laxmi502@gmail.com>
2019-05-05 13:38:57 +0300

    use prod url for accessing from kubernetes

--- CV/Program.cs
+++ CV/Program.cs
@@ -19,6 +19,7 @@ namespace CV
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://35.228.136.119")
                 .UseStartup<Startup>();
     }
 }
